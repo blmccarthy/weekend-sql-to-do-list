69a46852403bb6593d2b24ae3025f9b37dcf9205
Brant McCarthy <brant.mccarthy@me.com>
2022-02-12 16:40:00 -0600

    successfully tested getToDoList function

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,5 +1,47 @@
+// STARTUP CLIENT ----------------------------------------------
+
 $(readyNow);
 
 function readyNow(){
-    console.log('JQ');
+    getToDoList();
+}
+
+// GLOBAL VARS -------------------------------------------------
+
+// GET ---------------------------------------------------------
+
+function getToDoList(){
+    console.log('in getToDoList');
+    
+    $.ajax({
+        type: 'GET',
+        url: '/todo'
+    }).then(function(res){
+        console.log('in getToDoList .then', res);
+        renderToDom(res);
+    }).catch(function(err){
+        console.log('getToDoList .catch err:', err);
+    })
+}
+
+// POST --------------------------------------------------------
+
+// PUT ---------------------------------------------------------
+
+// DELETE ------------------------------------------------------
+
+// OTHER FUNCTIONS ---------------------------------------------
+
+function renderToDom(list){
+    console.log('in renderToDom');
+    $('#tbl-todo-list').empty();
+    for (let li of list){
+        $('#tbl-todo-list').append(`
+        <tr>
+            <td></td>
+            <td>${li.task_desc}</td>
+            <td><button>Delete</button></td>
+        <tr>
+        `);
+    }
 }
