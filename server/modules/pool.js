1ac1eebbe8214ef1c4f3201b617aa74751d7bd03
Brant McCarthy <brant.mccarthy@me.com>
2022-02-14 16:03:25 -0600

    updated pool.js for heroku compatibility

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -2,7 +2,7 @@ const pg = require('pg');
 const Pool = pg.Pool;
 
 const config = {
-  database: 'todo', 
+  database: process.env.DATABASE_NAME || 'todo', 
   host: 'localhost', 
   port: 5432, 
   max: 10, 
